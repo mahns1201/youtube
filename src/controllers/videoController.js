61b4aa4ecfb9f94eb4ecd939f3d48a1736f353a7
mahns <mahns1201@gmail.com>
2021-06-27 18:16:44 +0900

    [add] mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,40 @@
-// const fakeUser = {
-//     userName: "mahns",
-//     loggedIn: true,
-// };
+const fakeUser = {
+  userName: "mahns",
+  loggedIn: true,
+};
+
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+];
 
 export const trending = (req, res) => {
-    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    return res.render("home", { pageTitle: "Home", videos });
-}
+  return res.render("home", { pageTitle: "Home", videos, fakeUser: fakeUser });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
-
