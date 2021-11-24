f86f29b10e1594d326d9eaa766ad73a475ae76ef
서민혁 <mahns1201@gmail.com>
2021-11-25 00:33:07 +0900

    [feat] 3-1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./db/db";
+import "./models/Movie";
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import movieRouter from "./movieRouter";
+import { localsMiddleware } from "./middlewares";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(localsMiddleware);
+app.use("/", movieRouter);
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`✅  Server Ready!`));
