8835be7a94fab98124ce3f51bdc1a1add05baf54
서민혁 <mahns1201@gmail.com>
2021-11-24 23:46:50 +0900

    [init] 초기화

--- src/controllers/storiesController.js
+++
@@ -1,6 +0,0 @@
-export const home = (req, res) => res.render("home", { "pageTitle": "Home" });
-export const trending = (req, res) => res.render("trending", { "pageTitle": "Watch" });
-export const newStories = (req, res) => res.render("stories", { "pageTitle": "Stories" });
-export const seeStory = (req, res) => res.render("story", { "pageTitle": "Story" });
-export const editStory = (req, res) => res.render("editStory", { "pageTitle": "Edit Story" });
-export const deleteStory = (req, res) => res.render("deleteStory", { "pageTitle": "Delete Story" });
--- src/controllers/usersController.js
+++
@@ -1,5 +0,0 @@
-export const join = (req, res) => res.render("join", { "pageTitle": "Join" });
-export const login = (req, res) => res.render("login", { "pageTitle": "Login" });
-export const seeUsers = (req, res) => res.render("users", { "pageTitle": "See Users" });
-export const seeUser = (req, res) => res.render("user", { "pageTitle": "See User" });
-export const editProfile = (req, res) => res.render("editProfile", { "pageTitle": "Edit Profile" });
