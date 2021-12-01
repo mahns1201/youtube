f02fd614e5798e05d30ecab25eb9c1c7311d20fa
서민혁 <mahns1201@gmail.com>
2021-12-02 00:07:33 +0900

    [add] multer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./models/User";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import session from "express-session";
 import movieRouter from "./routers/movieRouter";
 import userRouter from "./routers/userRouter";
 import { localsMiddleware } from "./middlewares/setSiteTitle";
@@ -13,6 +14,22 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(localsMiddleware);
+
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
 app.use("/", movieRouter);
 app.use("/user", userRouter);
 
