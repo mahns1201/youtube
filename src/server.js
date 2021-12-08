254fb631d4310527d306eefef819e8db9d46ee13
서민혁 <mahns1201@gmail.com>
2021-12-09 00:41:11 +0900

    [feat] morgan

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
 const App = express();
 const PORT = 4000;
 const startMessage = () => console.log(`✅ Server listening on http://localhost:${PORT}`);
 
+const logger = morgan("dev");
+
+App.use(logger);
 App.get("/", (req, res) => (res.send("<h1>home</h1>")));
 
 App.listen(PORT, startMessage);
