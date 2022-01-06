911d7f56061ef528edec988c752f3aa162ad1247
서민혁 <mahns1201@gmail.com>
2022-01-06 19:40:14 +0900

    [feat] webpack-config
    + webpack.config.js
    + nodemon.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
-import MongoStore from 'connect-mongo';
+import MongoStore from "connect-mongo";
 
 // router
 import rootRouter from "./routers/rootRouter";
@@ -11,13 +11,11 @@ import videoRouter from "./routers/videoRouter";
 // middleware
 import { localMiddleware } from "./middlewares";
 
-
 // common
 const App = express();
 
 // middleware
 const logger = morgan("dev");
-
 
 App.set("view engine", "pug");
 App.set("views", process.cwd() + "/src/views");
@@ -26,19 +24,20 @@ App.use(logger);
 App.use(express.urlencoded({ extended: true }));
 
 App.use(
-    session({
-        secret: process.env.COOKIE_SECRETE,
-        resave: true,
-        saveUninitialized: false, // 세션이 수정될 때만 쿠키를 넘긴다.
-        // cookie: {
-        //     maxAge: 20000, // (ms)
-        // },
-        store: MongoStore.create({ mongoUrl: process.env.DB_URL })
-    })
+  session({
+    secret: process.env.COOKIE_SECRETE,
+    resave: true,
+    saveUninitialized: false, // 세션이 수정될 때만 쿠키를 넘긴다.
+    // cookie: {
+    //     maxAge: 20000, // (ms)
+    // },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
 );
 
 App.use(localMiddleware);
 App.use("/uploads", express.static("uploads")); // 노출시키고 싶은 폴더를 정의한다.
+App.use("/assets", express.static("assets"));
 App.use("/", rootRouter);
 App.use("/users", userRouter);
 App.use("/videos", videoRouter);
